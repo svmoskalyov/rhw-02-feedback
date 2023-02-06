30b7c55bcaf162c2271d2ede9a3b4f56d3b1327a
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-06 13:08:04 +0200

    Update Feedback.jsx

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -2,7 +2,7 @@ import { MdThumbUp, MdThumbsUpDown, MdThumbDown } from 'react-icons/md';
 import { Box } from './Box';
 import { Button } from './Button/Button';
 
-export const Feedback = () => {
+export const Feedback = ({ onGood, onNeutral, onBad }) => {
   return (
     <>
       <h1>Please leave feedback</h1>
@@ -13,9 +13,15 @@ export const Feedback = () => {
         justifyContent="space-evenly"
         my={4}
       >
-        <Button icon={MdThumbUp}>Good</Button>
-        <Button icon={MdThumbsUpDown}>Neutral</Button>
-        <Button icon={MdThumbDown}>Bad</Button>
+        <Button icon={MdThumbUp} click={onGood}>
+          Good
+        </Button>
+        <Button icon={MdThumbsUpDown} click={onNeutral}>
+          Neutral
+        </Button>
+        <Button icon={MdThumbDown} click={onBad}>
+          Bad
+        </Button>
       </Box>
     </>
   );
