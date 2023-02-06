cab36cd8124e3585c527046a98949b4ec8f17dc4
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-06 11:17:50 +0200

    Create Button.styled.jsx

---
+++ src/components/Button/Button.styled.jsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components';
+
+export const StyledButton = styled.button`
+  display: inline-flex;
+  align-items: center;
+  gap: ${p => p.theme.space[2]}px;
+  margin: ${p => p.theme.space[0]}px;
+  padding-top: ${p => p.theme.space[3]}px;
+  padding-bottom: ${p => p.theme.space[3]}px;
+  padding-left: ${p => p.theme.space[4]}px;
+  padding-right: ${p => p.theme.space[4]}px;
+  border: ${p => p.theme.borders.normal};
+  border-radius: ${p => p.theme.radii.normal};
+  background-color: ${p => p.theme.colors.primary};
+  color: ${p => p.theme.colors.white};
+  cursor: pointer;
+
+  :hover,
+  :focus {
+    background-color: ${p => p.theme.colors.secondary};
+  }
+`;
