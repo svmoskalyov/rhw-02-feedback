fe8cd1841b2145addba15aedb16ac7b91c29c0c1
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-07 13:27:56 +0200

    Update Box.jsx

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,4 +1,4 @@
 import styled from 'styled-components';
-import { color, space, layout, flexbox, border, shadow } from 'styled-system';
+import { space, layout } from 'styled-system';
 
-export const Box = styled('div')(color, space, layout, flexbox, border, shadow);
+export const Box = styled('div')(space, layout);
