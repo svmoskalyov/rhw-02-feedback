02c0fdd5db2ce4b434407b7c482ab28b01ddd6a7
Sergei Moskalyov <sv.moskalyov@gmail.com>
2023-02-07 12:12:23 +0200

    Update Statistics.styled.jsx

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,11 +1,11 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-family: ${p => p.theme.fonts.monospace};
-  font-size: ${p => p.theme.fontSizes.l};
-  font-weight: ${p => p.theme.fontWeights.bold};
-  line-height: ${p => p.theme.lineHeights.body};
-`;
+// export const Title = styled.h1`
+//   font-family: ${p => p.theme.fonts.monospace};
+//   font-size: ${p => p.theme.fontSizes.l};
+//   font-weight: ${p => p.theme.fontWeights.bold};
+//   line-height: ${p => p.theme.lineHeights.body};
+// `;
 
 export const SubTitle = styled.p`
   font-family: ${p => p.theme.fonts.body};
